81b03e19eb3d912bb70d43ebba0a5add316cfe44
Adil <mamyrkhanov.adil@gmail.com>
2022-02-28 19:17:53 +0600

    fixed user loading thought future builder

--- lib/getx/users/users_controller.dart
+++ lib/getx/users/users_controller.dart
@@ -1,19 +1,21 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
-
+import 'package:get/get.dart';
 import 'package:github_users/model/user.dart';
-import 'package:github_users/repo/persist/users_persist.dart';
-import 'package:github_users/repo/service/users_service.dart';
 import 'package:github_users/repo/users_repo.dart';
 
 class UsersController extends GetxController {
   UsersRepo repo;
 
-  List<User> users = [];
+  final users = Future.value(<User>[]).obs;
 
   UsersController({required this.repo});
 
   void getUsers() async {
-    users = await repo.getUsers();
-    update();
+    users.value = repo.getUsers();
+  }
+
+  @override
+  void onInit() {
+    getUsers();
+    super.onInit();
   }
 }
--- lib/getx/users/users_view.dart
+++ lib/getx/users/users_view.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:github_users/getx/users/users_controller.dart';
+import 'package:github_users/model/user.dart';
 
 class UsersView extends GetView<UsersController> {
   const UsersView({Key? key}) : super(key: key);
@@ -21,21 +20,32 @@ class UsersView extends GetView<UsersController> {
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: SingleChildScrollView(
-            child: Column(
-          children: users.map(
-            (user) {
-              final avatarUrl = user.avatarUrl == "" ? "https://dummyimage.com/40x40" : user.avatarUrl;
-              return ListTile(
-                leading: Image.network(avatarUrl),
-                title: Text(user.login),
-                subtitle: Text(user.id.toString()),
-                onTap: () {
-                  //user details page
-                },
-              );
-            },
-          ).toList(),
-        )),
+          child: FutureBuilder(
+            future: users.value,
+            builder: ((context, AsyncSnapshot<List<User>> snapshot) {
+              final usersList = snapshot.data;
+              if (usersList != null) {
+                return Column(
+                  children: usersList.map(
+                    (user) {
+                      final avatarUrl = user.avatarUrl == "" ? "https://dummyimage.com/40x40" : user.avatarUrl;
+                      return ListTile(
+                        leading: Image.network(avatarUrl),
+                        title: Text(user.login),
+                        subtitle: Text(user.id.toString()),
+                        onTap: () {
+                          //user details page
+                        },
+                      );
+                    },
+                  ).toList(),
+                );
+              }
+
+              return Container();
+            }),
+          ),
+        ),
       ),
     );
   }
